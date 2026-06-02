d6125870b2a44e601d9d1253d6c8816683a91cd3
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2026-06-03 00:47:25 +0200

    Add ValueRange type and associated methods for range operations

--- stat.go
+++ stat.go
@@ -1,1 +1,55 @@
 package goope
+
+type ValueRange struct {
+	Start int
+	End   int
+}
+
+func NewValueRange(start, end int) (ValueRange, error) {
+	if start > end {
+		return ValueRange{}, ErrInvalidRangeLimits
+	}
+	return ValueRange{Start: start, End: end}, nil
+}
+
+func (r ValueRange) Size() int {
+	return r.End - r.Start + 1
+}
+
+func (r ValueRange) Contains(value int) bool {
+	return value >= r.Start && value <= r.End
+}
+
+func (r ValueRange) Copy() ValueRange {
+	return ValueRange{Start: r.Start, End: r.End}
+}
+
+func sampleHGD(inRange, outRange ValueRange, nsample int, coins <-chan bool) int {
+	inSize := inRange.Size()
+	outSize := outRange.Size()
+	nSampleIndex := nsample - outRange.Start + 1
+
+	if inSize == outSize {
+		return inRange.Start + nSampleIndex - 1
+	}
+
+	inSampleNum := Rhyper(nSampleIndex, inSize, outSize-inSize, coins)
+	if inSampleNum == 0 {
+		return inRange.Start
+	}
+	return inRange.Start + inSampleNum - 1
+}
+
+func sampleUniform(inRange ValueRange, coins <-chan bool) int {
+	cur := inRange.Copy()
+	for cur.Size() > 1 {
+		mid := (cur.Start + cur.End) / 2
+		bit := <-coins
+		if bit == false {
+			cur.End = mid
+		} else {
+			cur.Start = mid + 1
+		}
+	}
+	return cur.Start
+}
--- util.go
+++ util.go
@@ -1,5 +1,6 @@
 package goope
 
+// bytesToBits converts a byte slice into a slice of bits (booleans), where each byte expands into 8 bits. The bits are ordered from the most significant bit to the least significant bit for each byte.
 func bytesToBits(b []byte) []bool {
 	bits := make([]bool, 0, len(b)*8)
 	for _, b := range b {
@@ -8,4 +9,11 @@ func bytesToBits(b []byte) []bool {
 		}
 	}
 	return bits
+}
+
+func boolToUint32(b bool) uint32 {
+	if b {
+		return 1
+	}
+	return 0
 }
